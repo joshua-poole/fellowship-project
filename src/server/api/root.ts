5a7668da7c5b8ad9e7726b96b1322e8888facef4
Josh Poole <joshpoole6@gmail.com>
2026-03-03 21:29:31 +1100

    fix: add routers into TRPC router creation to fix typeerror

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,21 +1,18 @@
+import { baseRouter } from "~/server/api/routers/base";
+import { tableRouter } from "~/server/api/routers/table";
+import { columnRouter } from "~/server/api/routers/column";
+import { rowRouter } from "~/server/api/routers/row";
+import { viewRouter } from "~/server/api/routers/view";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
 export const appRouter = createTRPCRouter({
+  base: baseRouter,
+  table: tableRouter,
+  column: columnRouter,
+  row: rowRouter,
+  view: viewRouter,
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
 
-/**
- * Create a server-side caller for the tRPC API.
- * @example
- * const trpc = createCaller(createContext);
- * const res = await trpc.post.all();
- *       ^? Post[]
- */
 export const createCaller = createCallerFactory(appRouter);
